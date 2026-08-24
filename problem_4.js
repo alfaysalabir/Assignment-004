efef6fe0cbf4c9454fcbb495cdf2768424a96683
alfaysalabir <alfaysalabir@gmail.com>
2026-08-24 13:14:49 +0600

    TypeScript Problem-04

---
+++ problem_4.js
@@ -0,0 +1,13 @@
+"use strict";
+/* ********* Problem 4 — Traffic Light Action ********* */
+function getTrafficAction(light) {
+    if (light === "red") {
+        return "Stop";
+    }
+    else if (light === "yellow") {
+        return "Slow Down";
+    }
+    else {
+        return "Go";
+    }
+}
